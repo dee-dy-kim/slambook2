a2650d81e4a32cc1e026e0f98438ddbc0ae46be2
Dee Kim <daydykim@snu.ac.kr>
2024-03-12 12:41:00 +0900

    create eigenMatrix.cpp for eigen library finding test

---
+++ ch6/eigenMatrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#include <ctime>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+#define MATRIX_SIZE 50
+
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
